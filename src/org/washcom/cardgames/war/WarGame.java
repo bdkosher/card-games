0ae9746e02944e02b8ec47e7e734aee16a7cc605
bdkosher <>
2012-08-04 15:07:48 -0400

    added a bit of randomness to fix infinite loop bug more games played by simulator and more statistics generated

--- src/org/washcom/cardgames/war/WarGame.java
+++ src/org/washcom/cardgames/war/WarGame.java
@@ -1,8 +1,8 @@
 package org.washcom.cardgames.war;
 
 import java.util.Comparator;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.Map;
 import org.washcom.cardgames.core.*;
 
 /**
@@ -15,11 +15,9 @@ public class WarGame {
      * The number of cards which each player plays in the event their played cards match.
      */
     public static final int WAR_FEE = 3;
-    
     private final Player player1;
     private final Player player2;
     private final Comparator<Card> chooser = new AcesHighCardComparator();
-    
     private boolean gameOver = false;
 
     public WarGame() {
@@ -45,7 +43,19 @@ public class WarGame {
         return gameOver;
     }
 
-    void battle(Deck pot, int fee) {
+    int battle() {
+        return battle(new Deck(), 0, 0);
+    }
+
+    /**
+     * Perform a battle.
+     *
+     * @param pot
+     * @param fee
+     * @param wars - the number of wars that have occurred so far in this recursive battle
+     * @return
+     */
+    int battle(Deck pot, int fee, int wars) {
         int cardsNeededToBattle = fee + 1;
         if (!player1.handHasAtLeast(cardsNeededToBattle)) {
             pot.put(player1.getHand().drawAll());
@@ -55,40 +65,68 @@ public class WarGame {
             pot.put(player2.getHand().drawAll());
             gameOver = true;
         }
-        
-        if (gameOver) return;
+
+        if (gameOver) {
+            return wars;
+        }
 
         pot.put(player1.getHand().draw(fee));
         pot.put(player2.getHand().draw(fee));
 
         Card card1 = player1.getHand().draw();
         Card card2 = player2.getHand().draw();
-        pot.put(card1);
-        pot.put(card2);
+        /*
+         * Some randomness is indeed necessary for some games to terminate.
+         */
+        if (Math.random() < 0.5d) {
+            pot.put(card1);
+            pot.put(card2);
+        } else {
+            pot.put(card2);
+            pot.put(card1);
+        }
         int result = chooser.compare(card1, card2);
         if (result < 0) {
             player2.getHand().putOnBottom(pot.drawAll());
+            return wars;
         } else if (result > 0) {
             player1.getHand().putOnBottom(pot.drawAll());
+            return wars;
         } else {
-            battle(pot, WAR_FEE);
+            return battle(pot, WAR_FEE, wars + 1);
+        }
+    }
+    
+    public class Result {
+        Player winner;
+        int totalBattles = 0;
+        /**
+         * Maps the number of wars which occurred during a battle (the key) to the number of battles
+         * which occurred during the game with that number of wars. More often than not, there is no
+         * war, which means that key=0 should have the highest value of the Map. The sum of all Map
+         * values equals the total number of battles.
+         */
+        final Map<Integer, Integer> totalWars = new HashMap<>();
+        private void recordBattle(int nbrOfWarsInBattle) {
+            ++totalBattles;
+            Integer wars = totalWars.get(nbrOfWarsInBattle);
+            totalWars.put(nbrOfWarsInBattle, (wars == null ? 0 : wars) + 1);
         }
     }
 
-    public int play() {
-        int totalNbrOfBattles = 0;
+    public Result play() {
+        Result result = new Result();
         while (!isGameOver()) {
-            ++totalNbrOfBattles;
-            battle(new Deck(), 0);
-            if (totalNbrOfBattles % 500 == 0) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                }
-                System.out.println("\n\t\t1: " + player1.getHand().size() + " 2: " + player2.getHand().size());
-                System.out.flush();
-            }
+            int nbrOfWarsInBattle = battle();
+            result.recordBattle(nbrOfWarsInBattle);
         }
-        return totalNbrOfBattles;
+        if (player1.getHand().isEmpty()) {
+            if (!player2.getHand().isEmpty()) {
+                result.winner = player2;
+            }
+        } else if (player2.getHand().isEmpty()) {
+            result.winner = player1;
+        }
+        return result;
     }
 }
--- src/org/washcom/cardgames/war/WarGameSimulator.java
+++ src/org/washcom/cardgames/war/WarGameSimulator.java
@@ -1,46 +1,110 @@
 package org.washcom.cardgames.war;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  *
  * @author Joe
  */
 public class WarGameSimulator {
-    
+
     private final List<Integer> gameLengths = new ArrayList<>();
-    
+    private final Map<Integer, List<Integer>> warCounts = new HashMap<>();
+
     public WarGameSimulator(int totalGamesToPlay) {
         for (int i = 0; i < totalGamesToPlay; ++i) {
-            System.out.print("Starting game ");
-            int length = new WarGame().play();
-            System.out.println("\n\tGame completed! " + length + " battles.");
-            gameLengths.add(length);
+//            System.out.print("Starting game...");
+            WarGame.Result result = new WarGame().play();
+//            System.out.println("completed! " + result.totalBattles + " battles.");
+            gameLengths.add(result.totalBattles);
+            for (Map.Entry<Integer, Integer> entry : result.totalWars.entrySet()) {
+                List<Integer> targetWarCounts = warCounts.get(entry.getKey());
+                if (targetWarCounts == null) {
+                    targetWarCounts = new ArrayList<>();
+                    warCounts.put(entry.getKey(), targetWarCounts);
+                }
+                targetWarCounts.add(entry.getValue());
+            }
         }
     }
     
-    int getLongestGameLength() {
-        return Collections.min(gameLengths);
+    int getTotalGamesPlayed() {
+        return gameLengths.size();
     }
-    
-    int getShortestGameLength() {
+
+    int getLongestGameLength() {
         return Collections.max(gameLengths);
     }
-    
+
+    int getShortestGameLength() {
+        return Collections.min(gameLengths);
+    }
+
     double getAverageGameLength() {
         int total = 0;
         for (Integer gameLength : gameLengths) {
             total += gameLength;
         }
-        return (double)total / (double)gameLengths.size();
+        return (double) total / (double) gameLengths.size();
     }
-    
+
+    int getMostWarsInGame(int nbrOfWarsPerBattle) {
+        if (nbrOfWarsPerBattle < 0) {
+            throw new IllegalArgumentException("Expecting a value of 0 or more.");
+        }
+        List<Integer> counts = warCounts.get(nbrOfWarsPerBattle);
+        return counts == null ? 0 : Collections.max(counts);
+    }
+
+    double getAverageWarsPerGame(int nbrOfWarsPerBattle) {
+        if (nbrOfWarsPerBattle < 0) {
+            throw new IllegalArgumentException("Expecting a value of 0 or more.");
+        }
+        List<Integer> targetWarCounts = warCounts.get(nbrOfWarsPerBattle);
+        if (targetWarCounts == null) {
+            return 0d;
+        }
+        int total = 0;
+        for (Integer warCount : targetWarCounts) {
+            total += warCount;
+        }
+        return (double) total / (double) gameLengths.size();
+    }
+
     public static void main(String[] args) {
-        WarGameSimulator simulator = new WarGameSimulator(100);
+        WarGameSimulator simulator = new WarGameSimulator(10000);
+        System.out.println("Total games played: " + simulator.getTotalGamesPlayed());
         System.out.println("Shortest game length: " + simulator.getShortestGameLength());
         System.out.println("Longest game length: " + simulator.getLongestGameLength());
         System.out.println("Average game length: " + simulator.getAverageGameLength());
+        for (int i = 0; i < 7; ++i) {
+            System.out.println("Most " + describeWar(i) + " in a game: " + simulator.getMostWarsInGame(i));
+            System.out.println("Average " + describeWar(i) + " per game: " + simulator.getAverageWarsPerGame(i));
+        }
+    }
+
+    static String describeWar(int warLength) {
+        switch (warLength) {
+            case 0:
+                return "warless battles";
+            case 1:
+                return "wars";
+            case 2:
+                return "double wars";
+            case 3:
+                return "triple wars";
+            case 4:
+                return "quadruple wars";
+            case 5:
+                return "quintuple wars";
+            case 6:
+                return "sextuple wars";
+            case 7:
+                return "septuple wars";
+            case 8:
+                return "octuple wars";
+            default:
+                return "x" + warLength + " wars";
+        }
     }
 }
