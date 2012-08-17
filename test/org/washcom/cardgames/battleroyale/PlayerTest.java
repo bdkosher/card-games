db737f67570e1915d97c5c6a3fe7eb3dc19e5627
bdkosher <>
2012-08-17 10:17:02 -0400

    more tests

--- test/org/washcom/cardgames/battleroyale/PlayerTest.java
+++ test/org/washcom/cardgames/battleroyale/PlayerTest.java
@@ -1,5 +1,7 @@
 package org.washcom.cardgames.battleroyale;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -12,6 +14,31 @@ import static org.washcom.cardgames.core.Denomination.*;
  * @author Joe
  */
 public class PlayerTest {
+    
+    private List<Card> allQueens = Arrays.asList(new Card[] {
+        new Card(QUEEN, HEARTS),
+        new Card(QUEEN, DIAMONDS),
+        new Card(QUEEN, SPADES),
+        new Card(QUEEN, CLUBS),
+    });
+    
+    private List<Card> allKings = Arrays.asList(new Card[] {
+        new Card(KING, HEARTS),
+        new Card(KING, DIAMONDS),
+        new Card(KING, SPADES),
+        new Card(KING, CLUBS)
+    });
+    
+    @Test
+    public void testSwapHandsWithSelf() {
+        Player self = new Player("me");
+        self.getHand().put(allKings);
+        self.swapHands(self);
+        assertEquals(4, self.getHand().size());
+        for (Card card : self.getHand().drawAll()) {
+            assertEquals(KING, card.getDenomination());
+        }
+    }
     
     @Test
     public void testSwapHandsTwoEmpty() {
@@ -53,6 +80,38 @@ public class PlayerTest {
         assertEquals(new Card(QUEEN, HEARTS), two.getHand().draw());
     }
     
+    @Test
+    public void testSwapNonEmpty() {
+        Player one = new Player("one");
+        one.getHand().put(allKings);
+        Player two = new Player("two");
+        two.getHand().put(allQueens);
+        one.swapHands(two);
+        assertEquals(4, one.getHand().size());
+        assertEquals(4, two.getHand().size());
+        for (Card card : one.getHand().drawAll()) {
+            assertEquals(QUEEN, card.getDenomination());
+        }
+        for (Card card : two.getHand().drawAll()) {
+            assertEquals(KING, card.getDenomination());
+        }
+    }
     
+    @Test
+    public void testSwapNonEmptySymmetric() {
+        Player one = new Player("one");
+        one.getHand().put(allKings);
+        Player two = new Player("two");
+        two.getHand().put(allQueens);
+        two.swapHands(one);
+        assertEquals(4, one.getHand().size());
+        assertEquals(4, two.getHand().size());
+        for (Card card : one.getHand().drawAll()) {
+            assertEquals(QUEEN, card.getDenomination());
+        }
+        for (Card card : two.getHand().drawAll()) {
+            assertEquals(KING, card.getDenomination());
+        }
+    }
     
 }
